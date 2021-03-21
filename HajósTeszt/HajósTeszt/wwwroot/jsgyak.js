c99827b1bac613c7329b093f4a2998327fc05a26
Patricia Németh <patricia.nemeth2@stud.uni-corvinus.hu>
2021-03-21 22:23:37 +0100

    jsgyak

---
+++ HajósTeszt/HajósTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,31 @@
+﻿window.onload = () => {
+
+    console.log("Betöltödött");
+
+    var faktorialisR = (n) => {
+        if (n === 0 || n === 1) {
+            return 1;
+
+        }
+        else {
+            return n * faktorialisR(n - 1);
+        }
+    }
+
+    for (var sor = 0; sor < 10; sor++) {
+        var sordiv = document.createElement("div");
+        sordiv.classList.add("sor");
+        document.getElementById("pascal").appendChild(sordiv);
+
+
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            var elemdiv = document.createElement("div");
+            elemdiv.classList.add("elem");
+            elemdiv.innerText = faktorialisR(sor) / faktorialisR(oszlop) * faktorialisR(sor - oszlop));
+            elemdiv.style.background = `rgb(0, 0, ${ 255* 2 / elemdiv.innerText) + 125
+        })`;
+    }
+
+}
+
+}
